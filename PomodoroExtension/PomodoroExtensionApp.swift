62cbe2e867079d8e49f340eaeca5e93426bc7aec
mbouabid25 <bouab22m@mtholyoke.edu>
2024-11-30 00:27:14 +0100

    Initial Commit

---
+++ PomodoroExtension/PomodoroExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroExtensionApp.swift
+//  PomodoroExtension
+//
+//  Created by Marwa Bouabid on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
